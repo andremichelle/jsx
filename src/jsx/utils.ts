7282ea43b75a5421ce4c837697f764d0ab7bef2b
André Michelle <andre.michelle@gmail.com>
2023-12-21 14:57:40 +0100

    removed obsolete parameter

--- src/jsx/utils.ts
+++ src/jsx/utils.ts
@@ -1,20 +1,19 @@
-import { Exec, Func } from "@common/lang.ts"
+import { Exec, Provider } from "@common/lang.ts"
 import { Inject } from "@jsx/inject.ts"
 
 export type HotspotUpdater = { update: Exec }
 
-export type HotSpotProps = { render: Func<HotspotUpdater, Element>, ref: Inject.Ref<HotspotUpdater> }
+export type HotSpotProps = { render: Provider<Element>, ref: Inject.Ref<HotspotUpdater> }
 
 export const Hotspot = ({ render, ref }: HotSpotProps) => {
-    let current: Element
+    let current: Element = render()
     const updater = {
         update: () => {
-            const next = render(updater)
+            const next = render()
             current.replaceWith(next)
             current = next
         }
     }
     ref.addTarget(updater)
-    current = render(updater)
     return current
 }
