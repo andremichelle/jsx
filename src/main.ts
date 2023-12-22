8e9153f08f15f8cb121ef2a89f17bffd3e55383b
André Michelle <andre.michelle@gmail.com>
2023-12-22 16:34:15 +0100

    Shows ExampleApp

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { IconLibrary } from "./icons.tsx"
-import { AudiotoolApp } from "./audiotool/app.tsx"
+import { ExampleApp } from "./example-app.tsx"
 
 (async () => {
     document.body.appendChild(IconLibrary)
-    // document.body.appendChild(ExampleApp())
-    document.body.appendChild(AudiotoolApp())
+    document.body.appendChild(ExampleApp())
+    // document.body.appendChild(AudiotoolApp())
 })()
