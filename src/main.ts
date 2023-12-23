95b5437fca1b454faf6407fd1771a82bb0840d2b
André Michelle <andre.michelle@gmail.com>
2023-12-23 19:34:00 +0100

    removes obsolete test api call

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import "./main.sass"
 import { IconLibrary } from "./icons.tsx"
 import { App } from "./audiotool/ui/App.tsx"
-import { fetchUserAlbumList } from "./audiotool/api.ts"
 
 (async () => {
-    console.log(await fetchUserAlbumList("sandburgen"))
-
     document.body.appendChild(IconLibrary)
     // document.body.appendChild(ExampleApp())
     document.body.appendChild(App())
